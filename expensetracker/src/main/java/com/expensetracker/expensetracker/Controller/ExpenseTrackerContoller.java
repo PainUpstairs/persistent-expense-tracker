3bcca8eb052c8cc5abb04ee9c2e57c617d95b372
Niraj Patil <niraj.anilpatil@gmail.com>
2024-02-25 01:03:14 +0530

    Project Created

---
+++ expensetracker/src/main/java/com/expensetracker/expensetracker/Controller/ExpenseTrackerContoller.java
@@ -0,0 +1,11 @@
+package com.expensetracker.expensetracker.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("expensetracker/rest/api/")
+public class ExpenseTrackerContoller {
+    
+}
